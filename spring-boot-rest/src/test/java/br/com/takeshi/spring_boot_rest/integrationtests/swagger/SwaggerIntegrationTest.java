c6a60069a5e7403a510da5c96f81f5ead81303aa
Aldenir dos Santos <89494499+AldenirTakeshi@users.noreply.github.com>
2026-05-31 10:50:46 -0400

    test: migra integration tests para MySQL local sem Testcontainers

---
+++ spring-boot-rest/src/test/java/br/com/takeshi/spring_boot_rest/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,20 @@
+package br.com.takeshi.spring_boot_rest.integrationtests.swagger;
+
+import br.com.takeshi.spring_boot_rest.config.TestConfigs;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest {
+
+    @Test
+    void shouldDisplaySwaggerPage() {
+        var content = RestAssured.given().basePath("/swagger-ui/index.html").port(TestConfigs.SERVER_PORT)
+                .when().get().then().statusCode(200).extract().body().asString();
+        assertTrue(content.contains("Swagger UI"));
+    }
+
+}
